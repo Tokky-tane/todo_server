25223933f504624a362d717c3af7e6608b335b6c
Tokky-tane <tokky_tane@outlook.com>
2019-06-30 15:38:24 +0900

    use db url for connect database

--- database.py
+++ database.py
@@ -1,14 +1,21 @@
 import psycopg2
 import os
 from flask import Flask, request, g
+from urllib.parse import urlparse
 
 
 def get_db():
     if 'db' not in g:
-        g.db = psycopg2.connect(dbname=os.environ['DB_NAME'],
-                                user=os.environ['DB_USER'],
-                                password=os.environ['DB_PASSWORD'],
-                                host=os.environ['DB_HOST'])
+        url = urlparse(os.environ['DB_URL'])
+        username = url.username
+        password = url.password
+        database = url.path[1:]
+        hostname = url.hostname
+        
+        g.db = psycopg2.connect(dbname=database,
+                                user=username,
+                                password=password,
+                                host=hostname)
     return g.db
 
 
