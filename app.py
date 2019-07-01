a07ed274c8e55b4209200361cab817fe5876d33d
Tokky-tane <tokky_tane@outlook.com>
2019-07-01 20:48:54 +0900

    change GET method response content-type to json

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ cred = credentials.Certificate({
     "type": os.environ['FIREBASE_TYPE'],
     "project_id": os.environ['FIREBASE_PROJECT_ID'],
     "private_key_id": os.environ['FIREBASE_PRIVATE_KEY_ID'],
-    "private_key": os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n','\n'),
+    "private_key": os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
     "client_email": os.environ['FIREBASE_CLIENT_EMAIL'],
     "client_id": os.environ['FIREBASE_CLIENT_ID'],
     "auth_uri": os.environ['FIREBASE_AUTH_URI'],
@@ -26,6 +26,7 @@ cred = credentials.Certificate({
     "client_x509_cert_url": os.environ['FIREBASE_CLIENT_CERT_URL']
 })
 firebase_admin.initialize_app(cred)
+
 
 @app.route('/users/<string:user_id>/tasks', methods=['GET', 'POST', 'DELETE'])
 def route_users_tasks(user_id):
@@ -40,8 +41,12 @@ def route_users_tasks(user_id):
         return '', status.HTTP_401_UNAUTHORIZED
 
     if request.method == 'GET':
-        tasks = get_user_tasks(decoded_token['uid'])
-        return tasks
+        tasks = get_user_tasks(user_id)
+
+        response = Response(mimetype='applicaiotn/json')
+        response.set_data(tasks)
+        return response
+
     elif request.method == 'POST':
         title = request.json['title']
         due_date = request.json['due_date']
@@ -50,12 +55,13 @@ def route_users_tasks(user_id):
             due_date = dateutil.parser.parse(due_date)
 
         id = create_task(user_id, title, due_date)
-        location = '/users/{}/tasks/{}'.format(decoded_token['uid'], id)
+        location = '/users/{}/tasks/{}'.format(user_id, id)
         response = create_post_response(location)
 
         return response
+
     else:
-        delete_user_tasks(decoded_token['uid'])
+        delete_user_tasks(user_id)
         return '', status.HTTP_200_OK
 
 
@@ -75,7 +81,11 @@ def route_task(user_id, task_id):
 
     if request.method == 'GET':
         task = get_task(task_id)
-        return task
+
+        response = Response(mimetype='application/json')
+        response.set_data(task)
+        return response
+
     elif request.method == 'PUT':
         title = request.json['title']
         due_date = request.json['due_date']
@@ -83,6 +93,7 @@ def route_task(user_id, task_id):
         update_task(task_id, title, due_date)
 
         return '', status.HTTP_200_OK
+
     else:
         delete_task(task_id)
         return '', status.HTTP_204_NO_CONTENT
